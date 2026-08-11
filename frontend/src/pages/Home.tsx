06a40a002e64f048cfa393ee2747a051f040ff9e
zzhaozhao1-max <zzhaozhao1@gmail.com>
2026-08-11 11:57:00 +0800

    Add dashboard home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfitDashboard from "../components/ProfitDashboard";
+
+export default function Home() {
+  return (
+    <main>
+      <ProfitDashboard />
+    </main>
+  );
+}
