94bfe9d82ee01458bfae3be33d2feb05f456423a
zzhaozhao1-max <zzhaozhao1@gmail.com>
2026-08-11 13:39:42 +0800

    Add chart analytics data service

---
+++ backend/app/analytics/chart_data.py
@@ -0,0 +1,14 @@
+# V2.9 Chart Analytics Data Service
+
+class ChartAnalytics:
+    def sales_trend(self):
+        return []
+
+    def profit_trend(self):
+        return []
+
+    def sku_ranking(self):
+        return []
+
+    def platform_summary(self):
+        return []
