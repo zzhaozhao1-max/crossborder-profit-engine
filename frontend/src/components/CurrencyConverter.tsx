d0c9cf17cfc2d7edc4be80c432e36bc396fcccda
zzhaozhao1-max <zzhaozhao1@gmail.com>
2026-08-11 11:40:22 +0800

    Add currency converter component prototype

---
+++ frontend/src/components/CurrencyConverter.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CurrencyConverter() {
+  return (
+    <div>
+      <h2>Currency Converter</h2>
+      <p>Supported currencies: USD, VND, PHP, MYR, THB, CNY</p>
+    </div>
+  );
+}
