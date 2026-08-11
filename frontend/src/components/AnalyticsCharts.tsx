c373be56e9b9882224297ca9c63a60a7df7562e2
zzhaozhao1-max <zzhaozhao1@gmail.com>
2026-08-11 13:39:34 +0800

    Add V2.9 analytics chart components

---
+++ frontend/src/components/AnalyticsCharts.tsx
@@ -0,0 +1,9 @@
+// V2.9 Analytics Charts
+// Sales trend, profit trend and SKU analytics chart components
+
+export const chartModules = {
+  salesTrend: true,
+  profitTrend: true,
+  skuRanking: true,
+  platformComparison: true
+};
